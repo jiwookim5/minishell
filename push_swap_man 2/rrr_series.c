43a5dd7c8ca40d25f5f534c2cd83f0b5aa77d26d
jiwookim5 <kimjiwoo8684@gmail.com>
2023-07-13 21:55:18 +0900

    dsfgnklsd

--- push_swap_man 2/rrr_series.c
+++ push_swap_man 2/rrr_series.c
@@ -6,32 +6,32 @@
 /*   By: jiwkim2 <jiwkim2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 21:59:58 by jiwkim2           #+#    #+#             */
-/*   Updated: 2023/07/12 22:00:49 by jiwkim2          ###   ########seoul.kr  */
+/*   Updated: 2023/07/13 18:26:50 by jiwkim2          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void rra(t_stack **a, int i)
+void    rra(t_stack **a, int i)
 {
-    t_node   *temp;
-	t_node   *last_front;
-	t_node   *stack;
+    t_node  *temp;
+	t_node  *last_front;
+	t_node  *stack;
 
 	stack = (*a)->top;
 	if (!(stack && stack->next))
-    	return ;
+        return ;
 	temp = stack;
 	while (temp->next)
 	{
-    	last_front = temp;
+        last_front = temp;
     	temp = temp->next;
 	}
 	temp->next = stack;
 	last_front->next = NULL;
 	(*a)->top = temp;
 	if (i != 1)
-    	write(1, "rra\n", 4);
+    	write (1, "rra\n", 4);
 }
 
 void rrb(t_stack **b, int i)
@@ -53,12 +53,12 @@ void rrb(t_stack **b, int i)
     last_front->next = NULL;
     (*b)->top = temp;
     if (i != 1)
-        write(1, "rrb\n", 4);
+        write (1, "rrb\n", 4);
 }
 
 void    rrr(t_stack **a, t_stack **b)
 {
-    rra(a, 1);
-    rrb(b, 1);
-    write(1, "rrr\n", 4);
+    rra (a, 1);
+    rrb (b, 1);
+    write (1, "rrr\n", 4);
 }
