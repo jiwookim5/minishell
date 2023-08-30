74c583578402b225a9aec949e30826c36277c32b
jiwookim5 <kimjiwoo8684@gmail.com>
2023-08-30 19:22:16 +0900

    shell

--- minishell/main.c
+++ minishell/main.c
@@ -1,13 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiwkim2 <jiwkim2@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/30 16:48:03 by jiwkim2           #+#    #+#             */
+/*   Updated: 2023/08/30 16:48:03 by jiwkim2          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-char	**g_envp = NULL;
-
-
-void	prompt(void)
+size_t	ft_strlen(const char *str)
 {
-	ft_putstr_fd("minishell-", STDERR_FILENO);
-	ft_putstr_fd(VERSION, STDERR_FILENO);
-	ft_putstr_fd("$ ", STDERR_FILENO);
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*res;
+
+	res = (t_list *)malloc(sizeof(t_list));
+	if (res == 0)
+		return (0);
+	res->content = content;
+	res->next = NULL;
+	return (res);
+}
+
+
+void init(t_list **node, t_parsing *info, char *line)
+{
+	info->p_i = 0;
+	info->i = 0;
+	info->quote = 0;
+	info->j = 0;
+	*node = ft_lstnew(NULL);
+	info->head = *node;
+
+}
+
+void set_quote(t_parsing *info, char quot)
+{
+	info->quote = quot;
+}
+
+void parsing_check(char *line, t_parsing *info)
+{
+	printf("line[i] : %c\n", line[info->i]);
+	printf("info->quote : %d\n", info->quote);
+	if (line[info->i] == info->quote)
+		set_quote(info, 0);
+	else if (info->quote == 0 && (line[info->i] == '\'' || line[info->i] == '\"'))
+		set_quote(info, line[info->i]);
+}
+t_list *parsing(char *line)
+{
+	t_list *node;
+	t_parsing info;
+
+	init(&node, &info, line);
+	while (line[info.i])
+	{
+		printf("sdf\n");
+		parsing_check(line, &info);//, node);
+		info.i++;
+	}
+	return(node);
 }
 
 int		minishell(void)
@@ -18,17 +83,19 @@ int		minishell(void)
 	line = NULL;
 	while (1)
 	{
-		prompt();
-		get_next_line(0, &line);
-		if (cmd_list = parsing_first(line))
+		line = readline("minishell $ ");
+		// printf("%s\n", line);
+		if (!line)
 		{
-			controller(cmd_list);
+			printf("error\n");
+			exit(0);
 		}
+		cmd_list = parsing(line);
 		free(line);
 	}
 }
 
-int		main(int argc, char **argv, char **envv)
+int		main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
--- minishell/minishell.h
+++ minishell/minishell.h
@@ -1,11 +1,12 @@
-#ifndef MINISHELL
-# define MINISHELL
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
 # include <stdlib.h>
 # include <string.h>
 # include <strings.h>
 # include <unistd.h>
-# include <unistd.h>
+# include <stdio.h>
+# include <readline/readline.h>
 
 
 typedef	struct	s_list
@@ -13,5 +14,19 @@ typedef	struct	s_list
 	void			*content;
 	struct s_list	*next;
 }				t_list;
+
+typedef struct	s_parsing
+{
+	int		p_i;
+	int		i;
+	int		j;
+	char	quote;
+	t_list	*head;
+}				t_parsing;
+
+
+int		main(int argc, char **argv);
+int		minishell(void);
+void	prompt(void);
 
 #endif
